45200e3a688b48252415b6fc56d7dc339ce1f45c
bitcoin34 <lorenrobinson428@gmail.com>
2016-01-27 00:26:59 +0000

    added def put

--- snippets.py
+++ snippets.py
@@ -3,4 +3,13 @@ import logging
 # Set the log output file, the log level
 logging.basicConfig(filename="snippets.log", level=logging.DEBUG)
 
-
+def put(name, snipett):
+    """
+    Store a snipett with an associated name.
+    
+    Returns the name and the snipett
+    """
+    logging.error("FIXME: Unimplemented - put({!r}, {!r})".format(name, snippet))
+    return name, snippet
+    
+    
